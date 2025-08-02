229ab01d533046be75e1a71389798e1b62f4589b
362053534 <362053534@users.noreply.github.com>
2025-08-02 14:47:09 +0000

    deploy: 19e889e4bc643a58a773bec02379822fb3c2bdd4

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"atahw_8h.html#a414747b879d890c1df55ccbd4208cf1ca8b2c6c7e319818c77569ecb3e7b0f981",
+"audsrv_8c.html#addc7abfb9fc4d953e5e6bd5014948a3e",
+"dir_0134b0376fbe77322088b496fb368f9b.html",
+"dir_7d0a621685e4738f4449cb73774f552c.html",
+"dir_ffd7594d846340e67d78bcacff900475.html",
+"ee_2network_2tcpip_2include_2ps2ip_8h.html",
+"ee__regs_8h.html#acd39cd79bea37ea375840a06097271e6",
+"file_xio_8h.html#structfxio__lseek64__packet",
+"globals_func.html",
+"group__packet2__types.html#a377674b9e78b2e038b15273f02d5c5bf",
+"gs__psm_8h.html#a203b8e3dcd8c7521eaa4ab803b7c190d",
+"ioctl_8h.html#ac4ae2f4426e2d20200af645891b8bced",
+"iop_2sound_2audsrv_2src_2rpc__server_8c.html#adc53fd5d7dc3f34aee63aef397616c74",
+"kernel_8h.html#a00681cc2c015383d12860ad09bf56394a784f566e9fb3d25d9f63259ee51677e8",
+"libcdvd-common_8h.html#structsce_cdl_f_i_l_e",
+"libgs_8h.html#ac0a06c6b1e02e2e71fced1c28991bd25",
+"libhdd_8h.html#acbd5ee9b3af51c40bbe440e01cd37d4c",
+"libpad_8c.html#a5c74670bbe8e8969d6ad3ea51f67610c",
+"mem_8h_source.html",
+"patch__enable__lmb_8c.html#ab869b0259adf3e07c89c791b1c32b41c",
+"ps2cam__rpc_8h.html#ad418786b63a1e7b442f387453567ad27",
+"s__scmva_8c_source.html",
+"smapregs_8h.html#a317e863475ceee1095a9f79fcafab0b9",
+"tcpip_8h.html#a116d7c5c8bf8531b27f8a9fa98e728c3",
+"usbd_8h.html#struct_usb_endpoint_descriptor"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
